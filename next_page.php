3e374f6b7c4e8a61d9ebdfaa73a6e698840af773
asnafadzil <103158428+asnafadzil@users.noreply.github.com>
2022-08-11 11:36:53 +0800

    h

---
+++ next_page.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+echo "<html>";
+
+$queryString = $_SERVER["QUERY_STRING"];
+echo "Query string of the incoming URL: ".$queryString."\n";
+echo "Cookies received:\n";
+ foreach ($_COOKIE as $name => $value) 
+{ echo " $name = $value\n";
+}
+$myLogin=$_SESSION["myLogin"];
+echo "Value of mylogin has been retrieved: ".$myLogin."\n";
+
+$myColor = $_SESSION["myColor"];
+echo "Value of myColor has been retrieved: ".$myColor."\n";
+
+echo "</html>\n";
+
+?>
